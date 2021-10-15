8d74293de33270fc1e844fc4655dae47fd9baecc
UharaShogo <56241858+UharaShogo@users.noreply.github.com>
2021-10-15 14:38:55 +0900

    add Intercept

---
+++ lib/intercept_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InterceptPage extends StatefulWidget {
+  const InterceptPage({Key? key}) : super(key: key);
+
+  @override
+  _InterceptPageState createState() => _InterceptPageState();
+}
+
+class _InterceptPageState extends State<InterceptPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
